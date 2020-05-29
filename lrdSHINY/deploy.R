2e0d83e162332a26a21c714640228c7f47f6e2d9
npm27 <nick.maxwell61@yahoo.com>
2020-05-29 12:55:35 -0500

    update shiny app location

--- lrdSHINY/deploy.R
+++ lrdSHINY/deploy.R
@@ -4,5 +4,4 @@
 #                          token = '857D9C76CFF94C26E71BE4E6A98ED9AD',
 #                          secret = 'e+cE4Qo5ydL3u/uDoa4jzy1a+qM5GEuhnSyiZ39N')
 
-rsconnect::deployApp('C:/Users/nickm.000/Documents/lrdSHINY')
-Y
+rsconnect::deployApp('C:/Users/nickm.000/Documents/GitHub/lrd/lrdSHINY')
