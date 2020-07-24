28df4d7c9d2a8efc5ef4d403fe6fbdc2ee37808d
npm27 <nick.maxwell61@yahoo.com>
2020-07-23 21:25:51 -0500

    small things

---
+++ free_recall_SHINY/Test Data RGN/rgn notes.R
@@ -0,0 +1,17 @@
+##Levenshtein of 1
+
+#CAT = .447 vs .48
+#Adhoc = .497 vs .50
+#Unrelated = .386 vs .38
+
+##Levenshtein of 2
+
+#CAT = .462 vs .48
+#Adhoc = .505 vs .50
+#Unrelated = .395 vs .38
+
+##Levenshtein of 3
+
+#CAT = .500 vs .48
+#Adhoc = .519 vs .50
+#Unrelated = .406 vs .38
