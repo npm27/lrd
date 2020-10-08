2749e23078a4714e3cb7306e99d0fa72e4182bef
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2020-10-08 14:48:14 -0400

    fixing example

--- R/prop_correct_sentence.R
+++ R/prop_correct_sentence.R
@@ -67,18 +67,16 @@
 #'
 #' DF_test <- read.csv("data/sentence_data.csv")
 #'
-#' scored_output <- prop_correct_sentence(responses = DF_test$Response,
-#'                                        key = DF_test$Sentence,
-#'                                        key.trial = DF_test$Trial.ID,
-#'                                        id = DF_test$Sub.ID,
-#'                                        id.trial = DF_test$Trial.ID,
-#'                                        cutoff = 1,
-#'                                        flag = TRUE,
-#'                                        group.by = DF_test$Condition,
-#'                                        group.by.names = "condition",
-#'                                        other = c(rep("stuff", nrow(DF_test))),
-#'                                        other.names = "fake_column",
-#'                                        token.split = " ")
+#'scored_output <- prop_correct_sentence(data = DF_test,
+#'  responses = "Response",
+#'  key = "Sentence",
+#'  key.trial = "Trial.ID",
+#'  id = "Sub.ID",
+#'  id.trial = "Trial.ID",
+#'  cutoff = 1,
+#'  flag = TRUE,
+#'  group.by = "Condition",
+#'  token.split = " ")
 #'
 #'head(scored_output$DF_Scored)
 #'
