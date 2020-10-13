ca8ac8edeb55ba8c1c5f18e1e27448f5c8d44d9c
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2020-10-13 10:09:20 -0500

    Update test prop correct cued.R
    Remove free recall functions from cued example

--- tests/test prop correct cued.R
+++ tests/test prop correct cued.R
@@ -14,7 +14,6 @@ source("R/prop_correct_cued.R")
 #flag = TRUE
 #group.by = "condition"
 
-#group.by and "" key answers
 scored_output <- prop_correct_cued(data = DF_test,
                                    responses = "response",
                                    key = "key",
@@ -31,36 +30,15 @@ head(scored_output$DF_Participant)
 
 head(scored_output$DF_Group)
 
-#group.by and $ key answers
-scored_output <- prop_correct_free(data = DF_test,
+##No group by
+scored_output <- prop_correct_cued(data = DF_test,
                                    responses = "response",
-                                   key = DF_test$key,
-                                   key.trial = DF_test$trial,
+                                   key = "key",
+                                   key.trial = "trial",
                                    id = "id",
                                    id.trial = "trial",
                                    cutoff = 1,
-                                   flag = TRUE,
-                                   group.by = "condition")
-
-head(scored_output$DF_Scored)
-
-head(scored_output$DF_Participant)
-
-head(scored_output$DF_Group)
-
-#Test multiple group.by
-#Make condition2 column
-DF_test$condition2 = c(rep("c", times = 8631), rep("d", times = 8632))
-
-scored_output <- prop_correct_free(data = DF_test,
-                                   responses = "response",
-                                   key = DF_test$key,
-                                   key.trial = DF_test$trial,
-                                   id = "id",
-                                   id.trial = "trial",
-                                   cutoff = 1,
-                                   flag = TRUE,
-                                   group.by = c("condition", "condition2"))
+                                   flag = TRUE)
 
 head(scored_output$DF_Scored)
 
