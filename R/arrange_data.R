9a8654f4b620ca184f64c24dc06173db64d21019
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2021-08-09 10:51:03 -0500

    fixing arrange data for multiple

--- R/arrange_data.R
+++ R/arrange_data.R
@@ -4,12 +4,20 @@
 #' responses are stored in the same cell and converts it to long format.
 #'
 #' @param data a dataframe of the variables you would like to return.
-#' Other variables will be included in the returned output in long format.
+#' Other variables will be included in the returned output in long format
+#' if they represent a one to one match with the participant ID. If you
+#' have repeated data, please use the repeated argument or run this
+#' function several times for each trial.
 #' @param responses a column name in the dataframe that contains
 #' the participant answers for each item in quotes (i.e., "column")
 #' @param sep a character separating each response in quotes - example: ",".
 #' @param id a column name containing participant ID numbers from
 #' the original dataframe
+#' @param repeated (optional) a single column name or set of columns
+#' that indicate repeated measures columns you would like to
+#' keep with the data. You should include all columns that are not a one
+#' to one match with the subject ID (i.e., participants saw multiple
+#' trials). Please see our vignette for an example.
 #'
 #' @return A dataframe of the participant answers including:
 #' \item{Sub.ID}{The participant id number}
@@ -35,37 +43,89 @@
 #'
 #' head(DF_long)
 #'
-arrange_data <- function(data, responses, sep, id){
+arrange_data <- function(data, responses, sep, id, repeated = NULL){
 
-  #get the data
-  data <- as.data.frame(data)
-  colnames(data)[grepl(id, colnames(data))] <- "Sub.ID"
+  if (is.null(repeated)){
 
-  #split the strings, returns a list
-  answers <- strsplit(data[ , responses], split = sep)
+    #get the data
+    data <- as.data.frame(data)
+    colnames(data)[grepl(id, colnames(data))] <- "Sub.ID"
 
-  #list to long
-  df <- data.frame(response = unlist(answers))
+    #split the strings, returns a list
+    answers <- strsplit(data[ , responses], split = sep)
 
-  #take out any extra spaces
-  df$response <- gsub("\\s+", " ", df$response)
-  df$response <- trimws(df$response, "both")
+    #list to long
+    df <- data.frame(response = unlist(answers))
 
-  #add participant id
-  df$Sub.ID <- rep(data$Sub.ID, unlist(lapply(answers, length)))
+    #take out any extra spaces
+    df$response <- gsub("\\s+", " ", df$response)
+    df$response <- trimws(df$response, "both")
 
-  #add position tag
-  df$position <- ave(df$Sub.ID, df$Sub.ID, FUN = seq_along)
+    #add participant id
+    df$Sub.ID <- rep(data$Sub.ID, unlist(lapply(answers, length)))
 
-  #add back in other columns that are one to one
-  other.columns <- setdiff(colnames(data),
-                           c("response", "Sub.ID", "position", responses))
-  for (col in other.columns){
-    data_temp <- unique(data[ , c("Sub.ID", col)])
-    if (sum(duplicated(data$Sub.ID)) == 0){
-      df <- merge(df, data_temp, by = "Sub.ID")
+    #add position tag
+    df$position <- ave(df$Sub.ID, df$Sub.ID, FUN = seq_along)
+
+    #add back in other columns that are one to one
+    other.columns <- setdiff(colnames(data),
+                             c("response", "Sub.ID", "position", responses))
+    for (col in other.columns){
+      data_temp <- unique(data[ , c("Sub.ID", col)])
+      if (sum(duplicated(data$Sub.ID)) == 0){
+        df <- merge(df, data_temp, by = "Sub.ID")
+      }
+
     }
-  }
+  } else {
+
+    #get the data
+    data <- as.data.frame(data)
+    colnames(data)[grepl(id, colnames(data))] <- "Sub.ID"
+
+    #create unique identifier
+    data$unique_ID <- apply(data[ , c("Sub.ID", repeated)],
+                            1, paste, collapse = "~~")
+
+
+    #split the strings, returns a list
+    answers <- strsplit(data[ , responses], split = sep)
+
+    #list to long
+    df <- data.frame(response = unlist(answers))
+
+    #take out any extra spaces
+    df$response <- gsub("\\s+", " ", df$response)
+    df$response <- trimws(df$response, "both")
+
+    #add unique ID
+    df$unique_ID <-rep(data$unique_ID, unlist(lapply(answers, length)))
+
+    #add position tag
+    df$position <- ave(df$unique_ID, df$unique_ID, FUN = seq_along)
+
+    #split columns back
+    df <- cbind(df, do.call('rbind',  strsplit(as.character(df$unique_ID), "~~", fixed = TRUE)))
+
+    #rename the new columns
+    colnames(df) <- c("response", "unique_ID", "position", "Sub.ID", repeated)
+
+    #drop unique id
+    df$unique_ID <- NULL
+    data$unique_ID <- NULL
+
+    #add back in other columns that are one to one
+    other.columns <- setdiff(colnames(data),
+                             c("response", "Sub.ID", "position", responses, repeated))
+    for (col in other.columns){
+      data_temp <- unique(data[ , c("Sub.ID", col)])
+      if (sum(duplicated(data$Sub.ID)) == 0){
+        df <- merge(df, data_temp, by = "Sub.ID")
+      }
+    }
+
+  } #end else
+
 
   return(df)
 }
