bc247b87707e23cdb978d1588b876930a41121a7
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2021-08-09 12:06:22 -0500

    finishing multiple

--- R/prop_correct_multiple.R
+++ R/prop_correct_multiple.R
@@ -50,7 +50,7 @@
 #' \item{DF_Participant}{A dataframe of the proportion correct by
 #' participant, which also includes optional z-scoring, grouping, and
 #' other variables.}
-#' \item{DF_group}{A dataframe of the summary scores by any optional
+#' \item{DF_Group}{A dataframe of the summary scores by any optional
 #' grouping variables, along with overall total proportion correct
 #' scoring.}
 #'
@@ -84,139 +84,46 @@
 #'
 prop_correct_multiple <- function(data, #data frame
                          responses, #participant responses
+                         key, #answers
                          key.trial, #free order
                          id, #participant id
                          id.trial, #free to participant match
                          cutoff = 0, flag = FALSE,
                          group.by = NULL){
 
-  #create data from inputs ----
+  #get list IDs
+  list_ids <- unique(data[ , id.trial])
 
-  #grab the input dataframe  and convert to our names
-  DF <- as.data.frame(data)
-  colnames(DF)[grepl(responses, colnames(DF))] <- "Responses"
-  colnames(DF)[grepl(id, colnames(DF))] <- "Sub.ID"
+  #split the data based on ID
+  data_list <- split(data, data[ , id.trial])
 
-  #create the scored data ----
+  answer_key <- data.frame("Answers" = key, "List.ID" = key.trial)
+  answer_list <- split(answer_key, answer_key$List.ID)
 
-  #create a scoring key, score each response once
-  answer_key <- data.frame("Responses" = as.character(),
-                           "Answer" = as.character())
+  #create a storage space for the final scored data
+  scored_data <- list()
 
-  #no need to check the same word twice
-  key <- unique(key)
-
-  #find the key-response pairs
-  for (i in unique(DF$Responses)) {
-
-    #Get the leven score
-    lev_score <- adist(i, key)
-    names(lev_score) <- key
-
-    #Find the minimum value for best match
-    #Figure out if the min score is within the cut off
-    if(min(lev_score) <= cutoff) {
-
-      #put that into the answer key
-      answer_key <- rbind(answer_key,
-                          c(i, attr(which.min(lev_score), "names")))
-
-    } else {
-
-      answer_key <- rbind(answer_key,
-                          c(i, NA))
-
-    }
-
+  #run the function on each list separately
+  for (i in 1:length(data_list)){
+    scored_data[[i]] <- prop_correct_free(data = data_list[[i]],
+                                          responses = responses,
+                                          key = answer_list[[i]]$Answers,
+                                          id = id,
+                                          cutoff = cutoff,
+                                          flag = flag,
+                                          group.by = group.by)
   }
 
-  #fix answer key
-  colnames(answer_key) <- c("Responses", "Answer")
+  #recreate the data
+  DF_Scored <- lapply(scored_data, `[[`, 1)
+  DF_Participant <- lapply(scored_data, `[[`, 2)
 
-  #with that answer key, score the data
-  DF <- merge(DF, answer_key, by = "Responses")
-  DF$Scored <- 1 - as.numeric(is.na(DF$Answer))
-
-  #create participant summary ----
-  k <- length(key)
-
-  #create participant data frame ----
   if (!is.null(group.by)){
+    DF_Group <- lapply(scored_data, `[[`, 3)
 
-      DF_participant <- aggregate(DF$Scored,
-                                  by = DF[ , c(group.by, "Sub.ID")],
-                                  function(x){sum(x)/k})
-      colnames(DF_participant) <- c(group.by, "Sub.ID", "Proportion.Correct")
-      } else {
-
-        DF_participant <- aggregate(DF$Scored, list(DF$Sub.ID), function(x){sum(x)/k})
-        colnames(DF_participant) <- c("Sub.ID", "Proportion.Correct")
-  }
-
-  #add back in other columns that are one to one
-  other.columns <- setdiff(colnames(DF),
-                           c("Responses", "Sub.ID", "Answer", "Scored",
-                             colnames(DF_participant)))
-  for (col in other.columns){
-    DF_temp <- unique(DF[ , c("Sub.ID", col)])
-    if (sum(duplicated(DF_temp$Sub.ID)) == 0){
-      DF_participant <- merge(DF_participant, DF_temp, by = "Sub.ID")
-    }
-  }
-
-  #if they want to flag participants ----
-  if (flag) {
-
-    #flag by group
-    if (!is.null(group.by)){
-
-      DF_participant$Z.Score.Group <- ave(DF_participant$Proportion.Correct,
-                                          DF_participant[ , group.by],
-                                          FUN = scale)
-
-    }
-      DF_participant$Z.Score.Participant <- scale(DF_participant$Proportion.Correct)
-
-  }
-
-  #group summary ----
-  #if they want a grouping variable
-  if (!is.null(group.by)){
-
-    #summarize participant scores by group
-    DF_group_person <- aggregate(DF$Scored,
-                                 by = DF[ , c(group.by, "Sub.ID")],
-                                 FUN = function(x){sum(x)/k})
-    colnames(DF_group_person) <- c(group.by,"Sub.ID", "Mean")
-
-    #why does aggregate do this
-    #if one variable by has to be a list
-    #if more than one, no list allowed
-    if (length(group.by) > 1){
-      DF_group <- aggregate(DF_group_person$Mean,
-                            by = DF_group_person[ , group.by], mean)
-      DF_group$SD <- aggregate(DF_group_person$Mean,
-                               by = DF_group_person[ , group.by], sd)$x
-      DF_group$N <- aggregate(DF_group_person$Mean,
-                              by = DF_group_person[ , group.by], length)$x
-    } else {
-      DF_group <- aggregate(DF_group_person$Mean,
-                            by = list(DF_group_person[ , group.by]), mean)
-      DF_group$SD <- aggregate(DF_group_person$Mean,
-                               by = list(DF_group_person[ , group.by]), sd)$x
-      DF_group$N <- aggregate(DF_group_person$Mean,
-                              by = list(DF_group_person[ , group.by]), length)$x
-    }
-
-    colnames(DF_group) <- c(group.by, "Mean", "SD", "N")
-
-    return(list(DF_Scored = DF,
-                DF_Participant = DF_participant,
-                DF_Group = DF_group))
-
+    return(list(DF_Scored, DF_Participant, DF_Group))
   } else {
-    return(list(DF_Scored = DF,
-                DF_Participant = DF_participant))
+    return(list(DF_Scored, DF_Participant))
   }
 
 }
